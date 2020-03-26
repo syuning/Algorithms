a2093cca784c254614a7517f7035030ddeb9bf71
syuning <syuning@vip.qq.com>
2020-03-26 16:00:21 +0800

    1

--- Leetcode探索/数组和字符串/java/二维数组/FindDiagonalOrder.java
+++ Leetcode探索/数组和字符串/java/二维数组/FindDiagonalOrder.java
@@ -1,78 +1,74 @@
 package Leetcode探索.数组和字符串.java.二维数组;
 
-import java.util.ArrayList;
-
 class FindDiagonalOrder {
-    public static int[] findDiagonalOrder(int[][] matrix) {
-        // 对角线遍历规律：
-        // 同一对角线内元素序列的索引[i][j]中的i和j正好相反
-        // 如在 n=8 的矩阵中，最长对角线的遍历，i的序列为 01234567，而j的序列为 76543210
-        // 因此矩阵的对角线序列，由长度为1开始到长度为 n-1，先递增再递减，i为递增时j为递减，反之亦然
+    public static int[] findDiagonalOrder (int[][] matrix) {
 
-
-        // 对角线的初始方向为 [0][0]至[0][1]，先向右再向左，因此i先递减再递增再递减，以此类推，而j正好相反，先递增再递减
-
-        int[][] iMatrix = new int [matrix.length * 2 - 1][]; //存储遍历的每一行
-        int[][] jMatrix = new int [matrix.length * 2 - 1][];
-
-        // 第n行之前
-        for (int i = 0; i < matrix.length; i++) {
-            iMatrix[i] = new int[i+1];
-            jMatrix[i] = new int[i+1];
-            if (i % 2 == 0) { // 偶数行
-                for (int j = 0; j < i + 1; j++) {
-                    iMatrix[i][j] = i - j;
-                    jMatrix[i][j] = j;
-                }
-            } else { // 奇数行
-                for (int j = 0; j < i + 1; j++) {
-                    iMatrix[i][j] = j;
-                    jMatrix[i][j] = i - j;
-                }
-            }
+        if (matrix == null || matrix.length == 0) {
+            return new int[0];
+        } else if (matrix.length == 1) {
+            return matrix[0];
         }
 
-        // 第n行之后
-        for (int i = matrix.length; i < matrix.length * 2 - 1; i++) {
-            iMatrix[i] = new int[matrix.length * 2 - i - 1];
-            jMatrix[i] = new int[matrix.length * 2 - i - 1];
-            if (i % 2 == 0) { // 偶数行
-                for (int j = 0; j < iMatrix[i].length; j++) {
-                    iMatrix[i][j] = matrix.length - j - 1;
-                    jMatrix[i][j] = i + 1 - matrix.length + j;
+        int width;
+        int height;
+        if (matrix.length > matrix[0].length) { // 较长的一边作为width
+            width = matrix.length;
+            height = matrix[0].length;
+        } else {
+            width = matrix[0].length;
+            height = matrix.length;
+        }
+        int numOfDia = (width + height) - 1;
+
+        //遍历前半部分对角线元素：
+        String seqOfJ = "", seqOfI = "";
+        for (int indOfDia = 0;  indOfDia < (numOfDia % 2 == 0 ? numOfDia / 2 : numOfDia / 2 + 1); indOfDia++) {
+            String indOfI = "", indOfJ = "";
+            if (indOfDia % 2 == 0) { // 偶数行：i递减-，j递增+
+                for (int diaLen = 0; diaLen < indOfDia + 1; diaLen++) { // 对角线的长度小于矩阵的较长边
+                    indOfI = diaLen + indOfI;
+                    indOfJ = indOfJ + diaLen;
                 }
-            } else { // 奇数行
-                for (int j = 0; j < iMatrix[i].length; j++) {
-                    iMatrix[i][j] = i + 1 - matrix.length + j;
-                    jMatrix[i][j] = matrix.length - j - 1;
+            } else { // 奇数行：i递增+，j递减
+                for (int diaLen = indOfDia; diaLen >= 0 ; diaLen--) { // 对角线的长度小于矩阵的较长边  
+                    indOfI = diaLen + indOfI;
+                    indOfJ = indOfJ + diaLen;  
                 }
             }
+            seqOfI += indOfI;
+            seqOfJ += indOfJ;
         }
 
-        // System.out.println();
-        // System.out.println();
-        // System.out.println("iMatrix: " + iMatrix.length);
-        // printMatrix(iMatrix);
-        // System.out.println();
-        // System.out.println("jMatrix: " + jMatrix.length);
-        // printMatrix(jMatrix);
-
-
-        ArrayList<Integer> seq = new ArrayList<>();
-        for (int i = 0; i < matrix.length * 2 - 1; i++) {
-            for (int j = 0; j < iMatrix[i].length; j ++) {
-                System.out.println(matrix[iMatrix[i][j]][jMatrix[i][j]] + " (i=" + i + " j=" + j + ")");
-                seq.add(matrix[iMatrix[i][j]][jMatrix[i][j]]);
+        //遍历后半部分对角线元素：
+        for (int indOfDia = numOfDia / 2 + 1;  indOfDia < numOfDia; indOfDia++) {
+            // System.out.println("indOfDia" + indOfDia);
+            String indOfI = "", indOfJ = "";
+                if (indOfDia % 2 == 0) {  // 偶数行：i递减-，j递增+
+                    for (int diaLen = width - 1; diaLen > indOfDia - width; diaLen--) {
+                    indOfI = indOfI + diaLen;
+                    indOfJ = diaLen + indOfJ;
+                }
+                } else { // 奇数行：i递增+，j递减
+                    for (int diaLen = indOfDia - width + 1; diaLen < width; diaLen++) {
+                    indOfI = indOfI + diaLen;
+                    indOfJ = diaLen + indOfJ;
+                }
             }
+            seqOfI += indOfI;
+            seqOfJ += indOfJ;
         }
+        System.out.println("seqOfI = " + seqOfI);
+        System.out.println("seqOfJ = " + seqOfJ);
 
-        int[] sequence = new int[seq.size()];
-        for (int i = 0; i < seq.size(); i++) {
-            sequence[i] = seq.get(i);
+        int[] result = new int[seqOfI.length()];
+        for (int i = 0; i < seqOfI.length(); i++) {
+            result[i] = matrix[seqOfI.charAt(i)-48][seqOfJ.charAt(i)-48];
         }
-        return sequence;
+        printArray(result);
+
+        return result;
     }
-    
+
     public static void printArray (int[] array) {
         System.out.println();
         System.out.println("This is the array: ");
@@ -81,21 +77,22 @@ class FindDiagonalOrder {
         }
         System.out.println();
     }
-    
-    private static void printMatrix(int[][] matrix) {
-        System.out.println();
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; matrix[i] != null && j < matrix[i].length; ++j) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.print("                             length: " + matrix[i].length);
-            System.out.println();
-        }
-    }
+
     public static void main(String[] args) {
         System.out.println(); System.out.println();
-        int[][] matrix3 = {{1, 2, 3} , {4, 5, 6}, {7, 8, 9} };
-        printArray(findDiagonalOrder(matrix3));
+
+        // int[][] matrix3 = {{1, 2, 3} , {4, 5, 6}, {7, 8, 9} };
+        // printArray(findDiagonalOrder(matrix3));
+        // [1,2,4,7,5,3,6,8,9]
+
+        int[][] matrix55 = {
+            { 1,  2,  3,  4,  5}, 
+            { 6,  7,  8,  9, 10}, 
+            {11, 12, 13, 14, 15}, 
+            {16, 17, 18, 19, 20}
+        };
+        findDiagonalOrder(matrix55);
+        // 1, 2, 6, 3, 7, 11, 4, 8, 12, 16, 5, 9, 13, 17, 10, 14, 18, 15, 19, 20
 
         // int[][] matrix7 = {
         //     { 0,  1,  2,  3,  4,  5,  6,  7},
@@ -108,5 +105,16 @@ class FindDiagonalOrder {
         //     {70, 71, 72, 73, 74, 75, 76, 77}
         // };
         // findDiagonalOrder(matrix7);
+
+        // int[][] matrix0 = {};
+        // printArray(findDiagonalOrder(matrix0));
+
+        // int[][] matrix23 = {{2, 3}};
+        // printArray(findDiagonalOrder(matrix23));
+
+        // int[][] matrix32 = { {3}, {2}};
+        // findDiagonalOrder(matrix32);
+
+        
     }
 }
