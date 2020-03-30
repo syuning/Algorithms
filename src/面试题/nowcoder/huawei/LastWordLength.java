9af37252a5c2ebde506584bb040e3051e3e94b2c
syuning <syuning@vip.qq.com>
2020-03-30 11:37:39 +0800

    init

---
+++ src/面试题/nowcoder/huawei/LastWordLength.java
@@ -0,0 +1,15 @@
+package 面试题.nowcoder.huawei;
+
+import java.util.Scanner;
+
+public class LastWordLength {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = "";
+        // while(input.hasNextLine()) {
+            s = input.nextLine();
+            System.out.println(s.length()-1-s.lastIndexOf(" "));
+        // }
+        input.close();
+    }
+}
