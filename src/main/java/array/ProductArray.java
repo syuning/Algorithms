06d871353673c6e73f8e42c716575175d2ffb2bb
songyuning <songyuning@inspur.com>
2020-08-05 16:50:34 +0800

    1

---
+++ src/main/java/array/ProductArray.java
@@ -0,0 +1,9 @@
+package main.java.array;
+
+public class ProductArray {
+    public int[] constructArr(int[] a) {
+        int[] b = new int[a.length];
+
+        return b;
+    }
+}
--- src/main/java/tree/BinaryTree.java
+++ src/main/java/tree/BinaryTree.java
@@ -26,4 +26,5 @@ public class BinaryTree {
         }
         return rightView;
     }
+
 }
