4fac285ae0cf4e8a5babb73a44406f2dd713ac0e
songyuning <songyuning@inspur.com>
2020-12-17 17:00:37 +0800

    12.17

--- src/main/java/nowcoder/huawei/HJ61.java
+++ src/main/java/nowcoder/huawei/HJ61.java
@@ -3,13 +3,25 @@ package main.java.nowcoder.huawei;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class HJ61 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s1, s2;
-        while ((s1 = br.readLine()) != null  && (s2 = br.readLine()) != null) {
+        String s;
+        while ((s = br.readLine()) != null) {
+            int numOfApples = Integer.parseInt(s.split(" ")[0]);
+            int numOfPlates = Integer.parseInt(s.split(" ")[1]);
+            int numOfChoices = 0;
 
+            for(int i = 0; i < numOfPlates; i++) {
+                // i个空盘子，即实际上只有 numOfPlates - i 个盘子里面有苹果
+                ArrayList<String> choices = new ArrayList<>();
+                for (int j = 0; j < numOfApples - i; j ++) {
+
+                }
+
+            }
         }
     }
 }
